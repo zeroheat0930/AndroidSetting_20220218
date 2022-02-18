a30b56cd1753699ce83d791eedb5553b0431d396
zeroheat0930 <dongjunkorea@naver.com>
2022-02-18 10:20:33 +0900

    Initial commit

---
+++ app/src/main/java/com/zeroheat/androidsetting_20220218/SplashActivity.kt
@@ -0,0 +1,11 @@
+package com.zeroheat.androidsetting_20220218
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+    }
+}
